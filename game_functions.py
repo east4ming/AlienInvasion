9583a51b50388a3d6c24d0c8fc0fdcaf7cba5693
east4ming <cuikaidong@foxmail.com>
2018-02-26 16:39:47 +0800

    20180223: 按"p"开始新游戏.

--- game_functions.py
+++ game_functions.py
@@ -13,7 +13,8 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         bullets.add(new_bullet)
 
 
-def check_keydown_events(event, ai_settings, screen, ship, bullets):
+def check_keydown_events(event, ai_settings, screen, ship, aliens, bullets,
+                         stats):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -24,6 +25,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         fire_bullet(ai_settings, screen, ship, bullets)
     elif event.key == pygame.K_q:
         sys.exit()
+    elif event.key == pygame.K_p:
+        if not stats.game_active:
+            start_game(ai_settings, screen, stats, ship, aliens, bullets)
 
 def check_keyup_events(event, ship):
     """响应松开"""
@@ -40,7 +44,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
+            check_keydown_events(event, ai_settings, screen, ship, aliens,
+                                 bullets, stats)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -54,17 +59,21 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
     """在玩家单击Play按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
-        # 隐藏光标
-        pygame.mouse.set_visible(False)
-        # 重置游戏统计信息
-        stats.reset_stats()
-        stats.game_active = True
-        # 清空外星人列表和子弹列表
-        aliens.empty()
-        bullets.empty()
-        # 创建一群新的外星人, 并让飞船居中
-        create_fleet(ai_settings, screen, ship, aliens)
-        ship.center_ship()
+        start_game(ai_settings, screen, stats, ship, aliens, bullets)
+
+
+def start_game(ai_settings, screen, stats, ship, aliens, bullets):
+    # 隐藏光标
+    pygame.mouse.set_visible(False)
+    # 重置游戏统计信息
+    stats.reset_stats()
+    stats.game_active = True
+    # 清空外星人列表和子弹列表
+    aliens.empty()
+    bullets.empty()
+    # 创建一群新的外星人, 并让飞船居中
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
 
 
 def update_screen(ai_settings, screen, stats, ship, game_character, aliens,
